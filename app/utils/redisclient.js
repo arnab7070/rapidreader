53133e139ce5be87b3834e5a312abad443d1d3ed
Arnab Nandi <arnabnandi7074@gmail.com>
2024-12-08 10:49:35 +0530

    changed redis client

--- app/utils/redisclient.js
+++ app/utils/redisclient.js
@@ -1,16 +1,13 @@
-import { createClient } from 'redis';
+const Redis = require("ioredis");
 
-export default async function ClientConnection() {
-    const Client = createClient({
-        username: process.env.REDIS_USERNAME,
-        password: process.env.REDIS_PASSWORD,
-        socket: {
-            host: process.env.REDIS_HOST,
-            port: process.env.REDIS_PORT
-        }
+export default function ClientConnection() {
+    const serviceUri = `rediss://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+    const redis = new Redis(serviceUri);
+    redis.on("connect", () => {
+        console.log("Redis Client Connected");
     });
-    Client.on('error', err => console.log('Redis Client Error', err));
-    await Client.connect();
-    console.log("Redis Client Connected");
-    return Client;
+    redis.on("error", (err) => {
+        console.log("Redis Client Error", err);
+    });
+    return redis;
 }
